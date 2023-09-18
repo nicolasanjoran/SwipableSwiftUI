552c9b566de1bee6d8fdb6862223cacd75cd99ba
Nicolas Anjoran <nicolas.anjoran@gmail.com>
2023-09-18 10:53:21 +0200

    initial commit

--- Sources/SwipableSwiftUI/SwipableSwiftUI.swift
+++ Sources/SwipableSwiftUI/SwipableSwiftUI.swift
@@ -1,6 +1,136 @@
-public struct SwipableSwiftUI {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
+import UIKit
 
-    public init() {
+fileprivate struct SwipableContentSizeCalculator: ViewModifier {
+    @Binding var size: CGSize
+    func body(content: Content) -> some View {
+        content
+            .background(
+                GeometryReader { proxy in
+                    Color.clear // we just want the reader to get triggered, so let's use an empty color
+                        .onAppear {
+                            size = proxy.size
+                        }
+                }
+            )
     }
 }
+
+fileprivate extension View {
+    func saveSize(in size: Binding<CGSize>) -> some View {
+        modifier(SwipableContentSizeCalculator(size: size))
+    }
+}
+
+struct Swipable<Content: View> : View {
+    private let content: Content
+    @State private var contentSize = CGSize()
+    
+    @State private var offset: CGFloat = 0
+    private let impactFeedbackGenerator = UIImpactFeedbackGenerator(style: .medium)
+    @State private var selection : HorizontalAlignment?
+    
+    private var leadingColor = Color.purple
+    private var trailingColor = Color.blue
+    private var leadingIcon = Image(systemName: "placeholdertext.fill")
+    private var trailingIcon = Image(systemName: "placeholdertext.fill")
+    private var leadingAction : (() -> Void)?
+    private var trailingAction : (() -> Void)?
+    
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    func swipeLeading(icon: Image, color: Color, action: @escaping ()->Void) -> Swipable {
+        var newView = self
+        newView.leadingIcon = icon
+        newView.leadingColor = color
+        newView.leadingAction = action
+        return newView
+    }
+    
+    func swipeTrailing(icon: Image, color: Color, action: @escaping ()->Void) -> Swipable {
+        var newView = self
+        newView.trailingIcon = icon
+        newView.trailingColor = color
+        newView.trailingAction = action
+        return newView
+    }
+
+    var body: some View {
+        VStack {
+            content
+                .background(Color.black.opacity(0.00000001))
+                .saveSize(in: $contentSize)
+                .overlay(
+                    ZStack {
+                        HStack {
+                            Spacer()
+                            leadingIcon
+                                .font(.title3)
+                                .padding()
+                                .foregroundColor(.white)
+                                .opacity(offset > 0 ? 1 : 0)
+                        }
+                        .frame(maxHeight: .infinity)
+                        .background(leadingColor)
+                        .offset(CGSize(width: -(contentSize.width), height: 0))
+                        HStack {
+                            trailingIcon
+                                .font(.title3)
+                                .padding()
+                                .foregroundColor(.white)
+                                .opacity(offset < 0 ? 1 : 0)
+                            Spacer()
+                        }
+                        .frame(maxHeight: .infinity)
+                        .background(trailingColor)
+                        .offset(CGSize(width: contentSize.width, height: 0))
+                    }
+                )
+                .offset(CGSize(width: offset, height: 0))
+                .highPriorityGesture(
+                    DragGesture(minimumDistance: 20, coordinateSpace: .local)
+                        .onChanged { v in
+                            var dragOffset = v.translation.width
+                            
+                            if leadingAction == nil {
+                                dragOffset = max(0, dragOffset)
+                            }
+                            if trailingAction == nil {
+                                dragOffset = min(0, dragOffset)
+                            }
+                            
+                            offset = dragOffset
+                            if selection == nil {
+                                if offset > contentSize.width / 2  {
+                                    selection = .leading
+                                    impactFeedbackGenerator.impactOccurred()
+                                } else if offset < -contentSize.width / 2  {
+                                    selection = .trailing
+                                    impactFeedbackGenerator.impactOccurred()
+                                }
+                            } else {
+                                if abs(offset) < (contentSize.width / 2 - 20) {
+                                    selection = nil
+                                    impactFeedbackGenerator.impactOccurred()
+                                }
+                            }
+                        }
+                        .onEnded { _ in
+                            withAnimation {
+                                if selection == .leading {
+                                    self.leadingAction?()
+                                } else if selection == .trailing {
+                                    self.trailingAction?()
+                                }
+                                offset = 0
+                                selection = nil
+                            }
+                        }
+                )
+        }.frame(maxWidth: .infinity)
+            .clipped()
+    }
+}
+
